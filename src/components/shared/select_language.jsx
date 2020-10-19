aa1cb49fcf79211f5bb6e1fa81775a4761af4b9e
Hassan <hmibrahim1999@gmail.com>
2020-10-19 20:59:40 +0200

    Add Dropdown icon

--- src/components/shared/select_language.jsx
+++ src/components/shared/select_language.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import classNames from 'classnames'
 import { useLang } from './lang_provider';
 
+const options = [
+  {label: "English", value: "en"},
+  {label: "中国酶", value: "zh"},
+];
+
 const SelectLanguage = ({isNetworkHeader, isOpen}) => {
+  const [isListOpen, setIsOpen] = useState(false);
   const [lang, setLang] = useLang();
-  return <a rel="noreferrer noopener" className={classNames('icon', { 'language': (!isNetworkHeader || isOpen), 'language--white': isNetworkHeader && !isOpen })} target='_blank'
-    onClick={() => setLang(prevLang => prevLang === 'en' ? 'zh' : 'en')} />;
+  const toggling = () => setIsOpen(!isListOpen);
+
+  const onOptionClicked = ({ value }) => () => {
+    setLang(value);
+    setIsOpen(false);
+  };
+
+  return (
+  <>
+    <a onClick={toggling} rel="noreferrer noopener" className={classNames('icon', { 'language': (!isNetworkHeader || isOpen), 'language--white': isNetworkHeader && !isOpen })} target='_blank'/>
+    {/* <div onClick={toggling} className='dropdown-header'>
+      {selectedOption.label}
+    </div> */}
+    <div className='dropdown-container'>
+      {isListOpen && (
+        <div className='dropdown-list-container'>
+          <ul className='dropdown-list'>
+            {options.map((option, index) => (
+              <li onClick={onOptionClicked(option)} key={index} className='list-item'>
+                {option.label}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  </>
+  );
 }
 
 export default SelectLanguage;
